7e7dd8e700118fd2c32edb2145bfcf4f43d11685
Michael Schmidt <mjschmidt@smcm.edu>
2022-02-27 14:07:20 -0500

    still learning goLang

--- main.go
+++ main.go
@@ -8,7 +8,17 @@ func main() {
 	var remainingTickets = 50
 
 	fmt.Printf("Weleome to the %v booking\n", conferenceName)
-	fmt.Println("We hope to see you in attendance", conferenceTickets, "tickets were created")
-	fmt.Println(remainingTickets, "tickets remain")
+	fmt.Printf("We hope to see you in attendance %v tickets were created\n", conferenceTickets)
+	fmt.Printf("%v tickets remain\n", remainingTickets)
+
+	var userName string
+	var tomTix int
+	// Tom's tocket order
+
+	fmt.Println("Enter your firstname:")
+	fmt.Scan(&userName)
+	tomTix = 2
+
+	fmt.Printf("%v got %v tickets", userName, tomTix)
 
 }
