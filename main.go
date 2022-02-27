85f68cfec0ed5c6555145aea5dfeaf52638e7355
Michael Schmidt <mjschmidt@smcm.edu>
2022-02-27 17:58:42 -0500

    adding functionality

--- main.go
+++ main.go
@@ -1,24 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Lightning Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Weleome to the %v booking\n", conferenceName)
 	fmt.Printf("We hope to see you in attendance %v tickets were created\n", conferenceTickets)
 	fmt.Printf("%v tickets remain\n", remainingTickets)
 
-	var userName string
-	var tomTix int
-	// Tom's tocket order
+	bookings := []string{}
 
-	fmt.Println("Enter your firstname:")
-	fmt.Scan(&userName)
-	tomTix = 2
+	for {
+		var firstName string
+		var ticketNumber uint
+		var lastName string
+		var email string
+		// Tom's tocket order
 
-	fmt.Printf("%v got %v tickets", userName, tomTix)
+		fmt.Println("Enter your firstname:")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your lastname:")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email:")
+		fmt.Scan(&email)
+
+		fmt.Println("How many tickets do you want?")
+		fmt.Scan(&ticketNumber)
+
+		remainingTickets = remainingTickets - ticketNumber
+		bookings = append(bookings, firstName+" "+lastName+",")
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The first names are: %v\n", firstNames)
+
+		//fmt.Printf("the slice: %v\n", bookings)
+
+		//fmt.Printf("%v %v got %v tickets sent to %v\n", firstName, lastName, ticketNumber, email)
+		//fmt.Printf("Thre are %v left\n", remainingTickets)
+
+	}
 
 }
